c12b057a9e344e366c8210f264b95118dcc1a61c
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:37:25 -0400

    Change organization to zopencommunity

--- cicd-dev.groovy
+++ cicd-dev.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/kotlinport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/kotlinport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/kotlinport.git'), string(name: 'PORT_DESCRIPTION', value: 'Kotlin is an open-source, statically typed programming language supported and developed by JetBrains and open-source contributors.' ), string(name: 'BUILD_LINE', value: 'DEV') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/kotlinport.git'), string(name: 'PORT_DESCRIPTION', value: 'Kotlin is an open-source, statically typed programming language supported and developed by JetBrains and open-source contributors.' ), string(name: 'BUILD_LINE', value: 'DEV') ]
   }
 }
